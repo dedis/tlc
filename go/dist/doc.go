3f77007966c782b73f7f4befdf3e8f4a121d510e
Bryan A Ford <brynosaurus@gmail.com>
2019-06-12 22:03:01 +0200

    more cleanup of partially-done stuff to avoid confusion

--- go/dist/doc.go
+++ go/dist/doc.go
@@ -1,4 +1,4 @@
-// This package implements a minimalistic network-based implementation
+// This package implements a minimalistic distributed implementation
 // of TLC and QSC for the non-Byzantine (fail-stop) threat model.
 // It uses TLS/TCP for communication, gob encoding for serialization, and
 // vector time and a basic gossip protocol for causal message propagation.
