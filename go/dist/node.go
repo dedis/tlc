ac2de5f12d51f0c719eba35fd62031b932d596ea
Jeff R. Allen <jeff.allen@epfl.ch>
2019-07-04 14:28:56 +0200

    go fmt

--- go/dist/node.go
+++ go/dist/node.go
@@ -4,57 +4,55 @@ import (
 	"sync"
 )
 
-
-var Threshold int		// TLC and consensus threshold
+var Threshold int // TLC and consensus threshold
 
 var MaxSteps int
-var MaxTicket int32 = 100	// Amount of entropy in lottery tickets
+var MaxTicket int32 = 100 // Amount of entropy in lottery tickets
 
-
-type Type int			// Type of message
+type Type int // Type of message
 const (
-	Prop Type = iota	// Raw unwitnessed proposal
-	Ack			// Acknowledgment of a proposal
-	Wit			// Threshold witness confirmation of proposal
+	Prop Type = iota // Raw unwitnessed proposal
+	Ack              // Acknowledgment of a proposal
+	Wit              // Threshold witness confirmation of proposal
 )
 
 type Message struct {
 	// Network/peering layer
-	From	int		// Which node originally sent this message
+	From int // Which node originally sent this message
 
 	// Causality layer
-	Seq	int		// Node-local sequence number for vector time
-	Vec	vec		// Vector clock update from sender node
+	Seq int // Node-local sequence number for vector time
+	Vec vec // Vector clock update from sender node
 
 	// Threshold time (TLC) layer
-	Step	int		// Logical time step this message is for
-	Typ	Type		// Message type
-	Prop	int		// Proposal Seq this Ack or Wit is about
-	Ticket	int32		// Genetic fitness ticket for this proposal
+	Step   int   // Logical time step this message is for
+	Typ    Type  // Message type
+	Prop   int   // Proposal Seq this Ack or Wit is about
+	Ticket int32 // Genetic fitness ticket for this proposal
 }
 
 type Node struct {
 	// Network/peering layer
-	self	int		// This node's participant number
-	peer	[]peer		// How to send messages to each peer
-	mutex	sync.Mutex	// Mutex protecting node's protocol stack
+	self  int        // This node's participant number
+	peer  []peer     // How to send messages to each peer
+	mutex sync.Mutex // Mutex protecting node's protocol stack
 
 	// Causal history layer
-	mat	[]vec		// Node's current matrix clock
-	oom	[][]*Message	// Out-of-order messages not yet delivered
-	seqLog	[][]*Message	// Nodes' message received and delivered by seq
-	saw	[]set		// Messages each node saw recently
-	wit	[]set		// Witnessed messages each node saw recently
+	mat    []vec        // Node's current matrix clock
+	oom    [][]*Message // Out-of-order messages not yet delivered
+	seqLog [][]*Message // Nodes' message received and delivered by seq
+	saw    []set        // Messages each node saw recently
+	wit    []set        // Witnessed messages each node saw recently
 
 	// Threshold time (TLC) layer
-	tmpl	Message		// Template for messages we send
-	save	int		// Earliest step for which we maintain history
-	acks	set		// Acknowledgments we've received in this step
-	wits	set		// Threshold witnessed messages seen this step
-	stepLog	[][]logEntry	// Nodes' messages seen by start of recent steps
+	tmpl    Message      // Template for messages we send
+	save    int          // Earliest step for which we maintain history
+	acks    set          // Acknowledgments we've received in this step
+	wits    set          // Threshold witnessed messages seen this step
+	stepLog [][]logEntry // Nodes' messages seen by start of recent steps
 
 	// This node's record of QSC consensus history
-	choice	[]choice	// Best proposal this node chose each round
+	choice []choice // Best proposal this node chose each round
 }
 
 type peer interface {
@@ -63,14 +61,14 @@ type peer interface {
 
 // Info each node logs about other nodes' views at the start of each time-step
 type logEntry struct {
-	saw	set		// All nodes' messages the node had seen by then
-	wit	set		// Threshold witnessed messages it had seen
+	saw set // All nodes' messages the node had seen by then
+	wit set // Threshold witnessed messages it had seen
 }
 
 // Record of one node's QSC decision in one time-step
 type choice struct {
-	best	int		// Best proposal this node chose in this round
-	commit	bool		// Whether node observed successful commitment
+	best   int  // Best proposal this node chose in this round
+	commit bool // Whether node observed successful commitment
 }
 
 func (n *Node) init(self int, peer []peer) {
@@ -80,4 +78,3 @@ func (n *Node) init(self int, peer []peer) {
 	n.initCausal()
 	n.initTLC()
 }
-
--- go/dist/qsc.go
+++ go/dist/qsc.go
@@ -1,9 +1,7 @@
 package dist
 
-
 // Witnessed QSC requires three TLC time-steps per consensus round.
 const RoundSteps = 3
-
 
 // The TLC layer upcalls this method on advancing to a new time-step,
 // with sets of proposals seen (saw) and threshold witnessed (wit) recently.
@@ -14,7 +12,7 @@ func (n *Node) advanceQSC(saw, wit set) {
 	// Calculate the starting step of the round that's just now completing.
 	s := n.tmpl.Step - RoundSteps
 	if s < 0 {
-		return	// Nothing to be done until the first round completes
+		return // Nothing to be done until the first round completes
 	}
 
 	// Find the best eligible proposal that was broadcast at s+0
@@ -22,7 +20,9 @@ func (n *Node) advanceQSC(saw, wit set) {
 	var bestProp *Message
 	var bestTicket int32
 	for p := range wit {
-		if p.Typ != Prop { panic("wit should contain only proposals") }
+		if p.Typ != Prop {
+			panic("wit should contain only proposals")
+		}
 		if p.Step == s+0 && p.Ticket >= bestTicket {
 			bestProp = p
 			bestTicket = p.Ticket
@@ -39,15 +39,15 @@ func (n *Node) advanceQSC(saw, wit set) {
 	//println(n.self, n.tmpl.Step, "choice", bestProp.From, "spoiled", spoiled, "reconfirmed", reconfirmed, "committed", committed)
 
 	// Don't bother saving history before the start of the next round.
-	n.save = s+1
+	n.save = s + 1
 }
 
 // Return true if there's another proposal competitive with a given candidate.
 func (n *Node) spoiledQSC(s int, saw set, prop *Message, ticket int32) bool {
 	for p := range saw {
 		if p.Step == s+0 && p.Typ == Prop && p != prop &&
-				p.Ticket >= ticket {
-			return true	// victory spoiled by competition!
+			p.Ticket >= ticket {
+			return true // victory spoiled by competition!
 		}
 	}
 	return false
@@ -55,11 +55,10 @@ func (n *Node) spoiledQSC(s int, saw set, prop *Message, ticket int32) bool {
 
 // Return true if given proposal was doubly confirmed (reconfirmed).
 func (n *Node) reconfirmedQSC(s int, wit set, prop *Message) bool {
-	for p := range wit {	// search for a paparazzi witness at s+1
+	for p := range wit { // search for a paparazzi witness at s+1
 		if p.Step == s+1 && n.stepLog[p.From][s+1].wit.has(prop) {
 			return true
 		}
 	}
 	return false
 }
-
