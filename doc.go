cbcbb9e3854c36be7dbba537dbd117af47513705
Bryan A Ford <brynosaurus@gmail.com>
2019-08-04 08:50:07 -0700

    Hmmm, maybe that wasn't necessary after all...

--- doc.go
+++
@@ -1,4 +0,0 @@
-// This repository implements Threshold Logical Clocks (TLC)
-// and Que Sera Consensus (QSC) in multiple languages.
-// For a full overview, see https://github.com/dedis/tlc/.
-package tlc
