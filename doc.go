d5c8f6118f724f3d0951b92f8e3db6a790246dc3
Bryan A Ford <brynosaurus@gmail.com>
2019-08-04 08:41:16 -0700

    Add placeholder doc.go files to top-level directories

---
+++ doc.go
@@ -0,0 +1,4 @@
+// This repository implements Threshold Logical Clocks (TLC)
+// and Que Sera Consensus (QSC) in multiple languages.
+// For a full overview, see https://github.com/dedis/tlc/.
+package tlc
