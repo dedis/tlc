c9cf7f77ab3ca453b0e56c3c65de7e4c948825ac
Bryan A Ford <brynosaurus@gmail.com>
2020-04-06 11:04:12 +0200

    atomic: fix bug, add random jitter to tests

--- go/model/qscod/fs/casfs/util/atomic_test.go
+++ go/model/qscod/fs/casfs/util/atomic_test.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"io/ioutil"
+	"math/rand"
 	"os"
 	"sync"
 	"testing"
-	"io/ioutil"
+	"time"
 )
 
 func TestWriteFileOnce(t *testing.T) {
@@ -12,6 +14,12 @@ func TestWriteFileOnce(t *testing.T) {
 	filename := "testfile.tmp"
 	var wg sync.WaitGroup
 	writer := func(i int) {
+
+		// Sleep a small random duration to jitter the test
+		time.Sleep(time.Duration(rand.Int63n(int64(time.Microsecond))))
+		println("thread",i,"writing")
+
+		// Try to write the file
 		b := make([]byte, i) // create i-length file filled with i's
 		for j := range b {
 			b[j] = byte(i)
@@ -28,6 +36,7 @@ func TestWriteFileOnce(t *testing.T) {
 		}
 
 		// Check that what we read back is valid
+		println("thread",i,"read",len(b))
 		i = len(b)
 		if i == 0 {
 			t.Error("zero-length file shouldn't be possible")
@@ -44,13 +53,13 @@ func TestWriteFileOnce(t *testing.T) {
 	// Test with increasing numbers of threads
 	for n := 1; n <= 128; n *= 2 {
 
-		t.Log(n, "threads")
+		println("\ntesting", n, "threads")
 		for i := 1; i <= n; i++ {
 			wg.Add(1)
 			go writer(i)
 		}
 		wg.Wait()
-	}
 
-	os.Remove(filename)
+		os.Remove(filename)
+	}
 }
