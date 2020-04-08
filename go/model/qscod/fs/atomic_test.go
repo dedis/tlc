1a3a8d9f235beb280cedfe3e6c4e5f7673c8784c
Bryan A Ford <brynosaurus@gmail.com>
2020-04-08 16:09:11 +0200

    casfs: move generic fs stuff up 2 levels

--- go/model/qscod/fs/atomic_test.go
+++ go/model/qscod/fs/atomic_test.go
@@ -17,7 +17,7 @@ func TestWriteFileOnce(t *testing.T) {
 
 		// Sleep a small random duration to jitter the test
 		time.Sleep(time.Duration(rand.Int63n(int64(time.Microsecond))))
-		println("thread",i,"writing")
+		//println("thread",i,"writing")
 
 		// Try to write the file
 		b := make([]byte, i) // create i-length file filled with i's
@@ -36,7 +36,7 @@ func TestWriteFileOnce(t *testing.T) {
 		}
 
 		// Check that what we read back is valid
-		println("thread",i,"read",len(b))
+		//println("thread",i,"read",len(b))
 		i = len(b)
 		if i == 0 {
 			t.Error("zero-length file shouldn't be possible")
@@ -53,7 +53,7 @@ func TestWriteFileOnce(t *testing.T) {
 	// Test with increasing numbers of threads
 	for n := 1; n <= 128; n *= 2 {
 
-		println("\ntesting", n, "threads")
+		//println("\ntesting", n, "threads")
 		for i := 1; i <= n; i++ {
 			wg.Add(1)
 			go writer(i)
