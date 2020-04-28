78016b5a770784b64970cd78876a935047e982d2
Bryan A Ford <brynosaurus@gmail.com>
2020-04-28 07:22:25 +0200

    QSCOD: another complete reforumulation, simplifying the Store interface to require only the last-stored value

--- go/model/qscod/cli.go
+++ go/model/qscod/cli.go
@@ -18,18 +18,33 @@ type Head struct {
 // A Store's keys are integer TLC time-steps,
 // and its values are Value structures.
 //
-// WriteRead(step, value) attempts to write value to the store at step,
-// returning the first value written by any client and Head{} for comh.
-// If the requested step has been aged out of the store,
-// returns the input value unmodified and the last committed Head.
+// WriteRead(step, value) attempts to write tv to the store at step v.P.Step,
+// returning the first value written by any client.
+// WriteRead may also return a value from any higher time-step,
+// if other clients have moved the store's state beyond v.P.Step.
+//
+// This interface intentionally provides no means to return an error.
+// If WriteRead encounters an error that might be temporary or recoverable,
+// then it should just keep trying (perhaps with appropriate backoff).
+// This is the fundamental idea of asynchronous fault tolerant consensus:
+// to tolerate individual storage node faults, persistently without giving up,
+// waiting for as long as it takes for the store to become available again.
+//
+// If the application encounters an error that warrants a true global failure,
+// then it should arrange for the Up function to return an error,
+// which will eventually cause all the worker threads to terminate.
+// In this case, the application should cancel the context passed to Run,
+// which will in turn cancel the contexts passed to active WriteRead calls,
+// which can simply return the value v that was requested to be written
+// in order to allow the per-node worker thread to terminate cleanly.
 //
 type Store interface {
-	WriteRead(step Step, value Value) (actual Value, comh Head)
+	WriteRead(v Value) Value
 }
 
 // Value represents the values that a consensus node's key/value Store maps to.
 type Value struct {
-	Pri  int64 // Random priority for this proposal
+	I    int64 // Random integer genetic fitness priority for this proposal
 	C, P Head  // Last-committed and newly-proposed history views
 	R, B Set   // Read set and broadcast set from TLCB
 }
@@ -42,181 +57,270 @@ type Set map[Node]Value
 // is uniquely the best, i.e., the set contains no history tied for best.
 func (S Set) best() (bn Node, bv Value, bu bool) {
 	for n, v := range S {
-		if v.Pri >= bv.Pri {
-			bn, bv, bu = n, v, v.Pri > bv.Pri
+		if v.I >= bv.I {
+			bn, bv, bu = n, v, v.I > bv.I
 		}
 	}
 	return bn, bv, bu
 }
 
-// client represents one logical client issuing transactions
-// to the consensus group and driving the QSC/TLC state machine forward
-// asynchronously across the n key/value stores.
-type client struct {
-	tr, ts int                     // TLCB thresholds
-	kv     []Store                 // Node state key/value stores
-	rv     func() int64            // Random priority generator
-	mut    sync.Mutex              // Mutex protecting this client's state
-	cond   *sync.Cond              // For awaiting threshold conditions
-	kvc    map[Step]map[Node]Value // Cache of key/value store values
-	rcom   Head                    // Committed history to return
+// Client represents a logical client that can propose transactions
+// to the consensus group and drive the QSC/TLC state machine forward
+// asynchronously across the key/value storesu defining the group's state.
+//
+// The caller must initialize the public variables
+// to represent a valid QSCOD configuration,
+// before invoking Client.Run to run the consensus algorithm.
+// The public configuration variables must not be changed
+// after starting the client.
+//
+// KV is a slice containing interfaces to each of the key/value stores
+// that hold the persistent state of each node in the consensus group.
+// The total number of nodes N is defined to be len(KV).
+//
+// Tr and Ts are the receive and spread thresholds, respectively.
+// To ensure liveness against up to F slow or crashed nodes,
+// the receive threshold must exclude the F failed nodes: i.e., Tr <= N-F.
+// To ensure consistency (safety), the constrant Tr+Ts > N must hold.
+// Finally, to ensure that each round enjoys a high probability
+// of successful commitment, it should be the case that N >= 3F.
+// Thus, given F and N >= 3F, it is safe to set Tr = N-F and Ts = N-Tr+1.
+// The precise minimum threshold requirements are slightly more subtle,
+// but this is a safe and simpler configuration rule.
+//
+// Up is a callback function that the Client calls regularly while running,
+// to update the caller's knowledge of committed transactions
+// and to update the proposal data the client attempts to commit.
+// Client passes to Up the Step number and proposal Data string
+// for some recent proposal that is known to have been committed.
+// This known-committed proposal will change regularly across calls,
+// but may not change on each call and may not even be monotonic.
+// The Up function returns a string representing the new preferred
+// proposal Data that the Client will subsequently attempt to commit.
+// The Up function also returns an error which, if non-nil,
+// causes the Client's operation to terminate and return that error.
+//
+// RV is a function to generate non-negatative random numbers
+// for the symmetry-breaking priority values QSCOD requires.
+// In a production system, these random numbers should have high entropy
+// for maximum performance (minimum likelihood of collisions),
+// and should be generated from a cryptographically strong private source
+// for maximum protection against denial-of-service attacks in the network.
+//
+type Client struct {
+	KV     []Store                    // Node state key/value stores
+	Tr, Ts int                        // TLCB receive and spread thresholds
+	Up     func(Head) (string, error) // Proposal update function
+	RV     func() int64               // Random priority generator
+
+	mut  sync.Mutex // Mutex protecting this client's state
+	prop string     // Preferred proposal defined by caller
 }
 
-// Commit starts a client with given configuration parameters,
+type work struct {
+	kvc  Set        // Key/value cache collected for this time-step
+	cond *sync.Cond // For awaiting threshold conditions
+	max  Value      // Value with highest time-step we must catch up to
+	next *work      // Forward pointer to next work item
+	err  error      // Non-nil indicates last work item
+}
+
+// Run starts a client running with its given configuration parameters,
+// proposing transactions and driving the consensus state machine continuously
+// until the caller requests that it shut down.
 // then requests the client to propose transactions containing
 // application-defined message msg until some message commits.
 // Returns the history that successfully committed msg.
-func Commit(tr, ts int, kvStores []Store, randomPri func() int64,
-	proposedData string, committed Head) Head {
+func (c *Client) Run() error {
 
-	// Initialize the client's synchronization and key/value cache state.
-	c := &client{tr: tr, ts: ts, kv: kvStores, rv: randomPri}
-	c.cond = sync.NewCond(&c.mut)
-	c.kvc = make(map[Step]map[Node]Value)
+	// Keep the mutex locked whenever we're not waiting.
+	c.mut.Lock()
+	defer c.mut.Unlock()
 
 	// Launch one client thread to drive each of the n consensus nodes.
-	for i := range kvStores {
-		go c.thread(Node(i), proposedData, committed.Step, committed)
+	lv, lw := Value{}, &work{kvc: make(Set), cond: sync.NewCond(&c.mut)}
+	for i := range c.KV {
+		go c.thread(Node(i), 0, lv, lw)
 	}
 
-	// Wait for some thread to discover the next committed history
-	c.mut.Lock()
-	for c.rcom.Step == 0 {
-		c.cond.Wait()
+	// Keep driving the consensus state forward until an error occurs.
+	for lw.err == nil {
+
+		// Create the next work item
+		lw.next = &work{kvc: make(Set), cond: sync.NewCond(&c.mut)}
+
+		// Wake up any threads waiting for it to appear
+		lw.cond.Broadcast()
+
+		// Wait for a threshold number of worker threads
+		// to complete the last work item
+		for len(lw.kvc) < c.Tr {
+			lw.cond.Wait()
+		}
+
+		// Update our current preferred proposal and error status
+		c.prop, lw.next.err = c.Up(lw.max.C)
+
+		// Move on to process the next work item
+		lw = lw.next
 	}
-	c.mut.Unlock()
 
 	// Any slow client threads will continue in the background
-	// until they catch up with the others and terminate.
-
-	// Return the next committed history we learned.
-	return c.rcom
+	// until they catch up with the others or successfully get cancelled.
+	return lw.err
 }
 
 // thread represents the main loop of a client's thread
 // that represents and drives a particular consensus group node.
-func (c *client) thread(node Node, pref string, step Step, lcom Head) {
+func (c *Client) thread(node Node, ls Step, lv Value, lw *work) {
+
 	c.mut.Lock() // Keep state locked while we're not waiting
 	defer c.mut.Unlock()
 
-	// Run until we find a commitment at a sufficiently high step number,
-	// and then until we're even with all the other client threads.
-	// Another thread might deadlock waiting for us if we finish too soon.
-	for c.rcom.Step == 0 || c.kvc[step] != nil {
+	// Process work-items defined by the main thread in sequence,
+	// terminating only when the main thread records an error.
+	for lw.err == nil {
 
-		// Prepare a proposal containing the message msg
-		// that this client would like to commit,
-		// and invoke TLCB to (try to) issue that proposal on this node.
-		v0 := Value{Pri: c.rv(), C: lcom, P: Head{step, pref}}
-		v0, R0, B0 := c.tlcb(node, step, v0)
+		// Collect a threshold number of last-step values in lw.kvc,
+		// after which work-item lw will be considered complete.
+		// Don't modify kvc or max after reaching the threshold tr,
+		// because other code expects these to be immutable afterwards.
+		if len(lw.kvc) < c.Tr {
 
-		// Invoke TLCB again to re-broadcast the best eligible proposal
-		// we see in the B0 returned from the first TLCB instance,
-		// in attempt to reconfirm (double-confirm) that proposal
-		// so that all nodes will *know* that it's been confirmed.
-		_, v2, _ := B0.best() // some best confirmed proposal from B0
-		v2.R, v2.B = R0, B0
-		v2, R2, B2 := c.tlcb(node, step+2, v2)
-		R0, B0 = v2.R, v2.B // correct our state from v2 read
+			// Save the actual value read into our local cache
+			lw.kvc[node] = lv
 
-		// Unconditionally adopt the best confirmed proposal from R2
-		// as our own new (still tentative so far) view of history.
-		_, v4, _ := R2.best() // some best confirmed proposal from R2
+			// Track the highest last-step value read on any node,
+			// which may be higher than the one we tried to write
+			// if we need to catch up with a faster node.
+			if lv.P.Step > lw.max.P.Step {
+				lw.max = lv
+			}
 
-		// We can't (yet) know for certain if this view is committed,
-		// but the last commit it records definitely is committed.
-		// Return that as the next commit if we hadn't yet seen it.
-		if v4.C.Step > lcom.Step && c.rcom.Step == 0 {
-			c.rcom = v4.C      // record that v4.C is committed
-			c.cond.Broadcast() // signal the main thread
+			if lv.P.Step < ls || lv.P.Step > lw.max.P.Step {
+				panic("XXX")
+			}
 		}
 
-		// Finally, see if we can determine v4 to have been committed,
-		// which happens if v4 is the uniquely-best eligible proposal.
-		// This test may succeed only for some nodes even in a round.
-		n, b, u := R0.best() // is there a uniquely-best proposal in R0?
-
-		// If b is uniquely-best in R0, we can compare priorities
-		// to determine if two values are the same node's proposal.
-		if u && b.Pri == v4.Pri && b.Pri == B2[n].Pri &&
-			c.rcom.Step == 0 && b.P.Step > lcom.Step {
-			c.rcom = b.P       // record that b is committed
-			c.cond.Broadcast() // signal the main thread
+		// Wait until the main thread has created a next work item,
+		// and until we have reached the receive threshold.
+		for lw.next == nil || len(lw.kvc) < c.Tr {
+			lw.cond.Wait()
 		}
 
-		step += 4 // Two TLCB instances took two time-steps each
+		// Wake up everyone else once we reach the receive threshold
+		lw.cond.Broadcast()
+
+		// Decide on the next step number and value to broadcast,
+		// based on the threshold set we collected,
+		// which is now immutable and consistent across threads.
+		s, v := ls+1, Value{C: lv.C, P: Head{Step: ls + 1}}
+		switch {
+		case lw.max.P.Step > ls:
+
+			// The last work-item failed to reach the threshold
+			// because some node had already reached a higher step.
+			// Our next work item is simply to catch up all nodes
+			// at least to the highest-known step we discovered.
+			s, v = lw.max.P.Step, lw.max
+
+		case (ls & 1) == 0: // completing an even-numbered step
+
+			// Complete the first TLCR broadcast
+			// and start the second within a TLCB round.
+			// The value for the second broadcsast is simply
+			// the (any) threshold receive set from the first.
+			v.R = lw.kvc
+
+		case (ls & 3) == 1:
+
+			// Complete the first TLCB call in a QSCOD round
+			// and start the second TLCB call for the round.
+
+			// Calculate valid potential (still tentative)
+			// R and B sets from the first TLCB call in this round,
+			// and include them in the second TLCB broadcast.
+			R0, B0 := c.tlcbRB(lw.kvc)
+
+			// Pick any best confirmed proposal from B0
+			// as this node's broadcast for the second TLCB round.
+			_, v2, _ := B0.best()
+
+			// Set the value for the second TLCB call to broadcast
+			v.I, v.R, v.B = v2.I, R0, B0
+
+		case (ls & 3) == 3:
+
+			// Complete a prior QSCOD round and start a new one.
+
+			// First, calculate valid potential R2 and B2 sets from
+			// the second TLCB call in the completed QSCOD round.
+			R2, B2 := c.tlcbRB(lw.kvc)
+
+			// We always adopt some best confirmed proposal from R2
+			// as our own (still tentative so far) view of history.
+			_, b2, _ := R2.best()
+
+			// Find the best proposal b0 in some node's R0 set.
+			// We can get an R0 set from the first round in b2.R.
+			// Also determine if b0 was uniquely best in this R0.
+			// Our R2 and B2 sets will be subsets of any valid R0.
+			n0, b0, u0 := b2.R.best()
+
+			// See if we can determine b2 to have been committed:
+			// if b0==b2 is the uniquely-best eligible proposal.
+			// This test may succeed only for some nodes in a round.
+			// If b is uniquely-best in R0 we can compare priorities
+			// to see if two values are the same node's proposal.
+			if u0 && b0.I == b2.I && b0.I == B2[n0].I {
+				v.C = b0.P // original proposal including data
+			}
+
+			// Set the value for the first TLCB call
+			// in the next QSCOD round to broadcast,
+			// containing a proposal for the next round.
+			v.P.Data, v.I = c.prop, c.RV()
+		}
+
+		// Try to write new value, then read whatever the winner wrote.
+		// But don't write any state changes if our kvc has been
+		// contaminated with values from future time-steps.
+		c.mut.Unlock()
+		v = c.KV[node].WriteRead(v)
+		c.mut.Lock()
+
+		// Note that the newly-returned value v
+		// may be from a higher time-step than expected (s).
+		// We'll deal with that above in the next iteration.
+
+		// Proceed to the next work item
+		ls, lv, lw = s, v, lw.next
 	}
 }
 
-// tlcb implements the TLCB algorithm for full-spread threshold broadcast,
-// each requiring two TLCR invocations and hence two TLC time-steps.
+// tlcbRB calculates the receive (R) and broadcast (B) sets
+// returned by the TLCB algorithm after its second TLCR call.
 //
-// The provided v0 represents a potential next state for this node,
-// but other clients may of course race with this one to set the next state.
-// The returned Value represents the next-state value successfully registered
-// by whatever client won this race to initiate TLCB at step s.
-//
-// The returned R and B sets, in contrast, are tentative,
+// The returned R and B sets are only tentative,
 // representing possible threshold receive-set and broadcast-set outcomes
 // from this TLCB invocation, computed locally by this client.
 // These locally-computed sets cannot be relied on to be definite for this node
 // until the values computed from them are committed via Store.WriteRead.
 //
-func (c *client) tlcb(node Node, step Step, v0 Value) (Value, Set, Set) {
-
-	// First invoke TLCR to (try to) record the desired next-state value,
-	// and record the definite winning value and a tentative receive-set.
-	v0, v0r := c.tlcr(node, step+0, v0)
-
-	// In the second TLCR invocation,
-	// broadcast the set of proposals we received in the first.
-	_, v1r := c.tlcr(node, step+1, Value{R: v0r})
+func (c *Client) tlcbRB(kvc Set) (Set, Set) {
 
 	// Using the tentative client-side receive-set from the second TLCR,
 	// compute potential receive-set (R) and broadcast-set (B) sets
 	// to return from TLCB.
-	R, B, Bc := make(Set), make(Set), make([]int, len(c.kv))
-	for _, v := range v1r {
+	R, B, Bc := make(Set), make(Set), make([]int, len(c.KV))
+	for _, v := range kvc {
 		for j, vv := range v.R {
 			R[j] = vv          // R has all values we've seen
 			Bc[j]++            // How many nodes have seen vv?
-			if Bc[j] >= c.ts { // B has only those reaching ts
+			if Bc[j] >= c.Ts { // B has only those reaching ts
 				B[j] = vv
 			}
 		}
 	}
-	return v0, R, B
-}
-
-// tlcr implements the TLCR algorithm for receive-threshold broadcast,
-// each requiring a single TLC time-step.
-//
-func (c *client) tlcr(node Node, step Step, v Value) (Value, Set) {
-
-	// Create our key/value cache map for step s if not already created
-	if _, ok := c.kvc[step]; !ok {
-		c.kvc[step] = make(map[Node]Value)
-	}
-
-	// Try to write potential value v, then read that of the client who won.
-	// If we found a committed history, either ourselves or virally,
-	// then just pretend to do writes until we can synchronize and finish.
-	// We also might need to skip over versions when writing to catch up.
-	if c.rcom.Step == 0 {
-		v, c.rcom = c.kv[node].WriteRead(step, v)
-	}
-
-	// Wait until tr client threads insert values into kvc[s],
-	// waking up all waiting threads once this condition is satisfied.
-	// Don't modify kvc[step] after it reaches the threshold tr,
-	// because the code above expects threshold sets to be immutable.
-	for len(c.kvc[step]) < c.tr {
-		c.kvc[step][node] = v // save value v in our local cache
-		if len(c.kvc[step]) < c.tr {
-			c.cond.Wait() // wait to reach receive threshold
-		} else { // threshold reached
-			c.cond.Broadcast() // wake up waiting threads
-		}
-	}
-	return v, c.kvc[step] // return the satisfying value set we found
+	return R, B
 }
--- go/model/qscod/cli_test.go
+++ go/model/qscod/cli_test.go
@@ -4,35 +4,26 @@ import "fmt"
 import "math/rand"
 import "sync"
 import "testing"
+import "errors"
 
 // Trivial intra-process key-value store implementation for testing
 type testStore struct {
-	mut  sync.Mutex     // synchronization for testStore state
-	kv   map[Step]Value // the key/value map
-	lcom Head           // latest history known to be committed
+	mut sync.Mutex // synchronization for testStore state
+	v   Value      // the latest value written
 }
 
 // WriteRead implements the Store interface with a simple intra-process map.
-func (ts *testStore) WriteRead(step Step, v Value) (Value, Head) {
+func (ts *testStore) WriteRead(v Value) Value {
 	ts.mut.Lock()
 	defer ts.mut.Unlock()
 
-	// If the requested step s is too old, return last committed history
-	if step < ts.lcom.Step {
-		return v, ts.lcom
+	// Write value v only if it's newer than the last value written.
+	if v.P.Step > ts.v.P.Step {
+		ts.v = v
 	}
 
-	// Write value v if not already written, then return the actual value.
-	if _, ok := ts.kv[step]; !ok { // no client wrote a value yet for s?
-		ts.kv[step] = v // write-once
-
-		// Update our record of the last-known committed history,
-		// to simulate garbage-collection for testing purposes.
-		if ts.lcom.Step < v.C.Step {
-			ts.lcom = v.C
-		}
-	}
-	return ts.kv[step], Head{} // Return the winning value in any case
+	// Then return whatever was last written, regardless.
+	return ts.v
 }
 
 // Object to record the common total order and verify it for consistency
@@ -62,36 +53,50 @@ func (to *testOrder) committed(t *testing.T, h Head) {
 }
 
 // testCli creates a test client with particular configuration parameters.
-func testCli(t *testing.T, self, nfail, ncom, maxpri int,
+func testCli(t *testing.T, self, f, maxstep, maxpri int,
 	kv []Store, to *testOrder, wg *sync.WaitGroup) {
 
+	// Use the simple Int63n for random number generation,
+	// with values constrained to be lower than maxpri for testing.
+	// A real deployment should use cryptographic randomness
+	// and should preferably be high-entropy, close to the full 64 bits.
 	rv := func() int64 { return rand.Int63n(int64(maxpri)) }
 
-	// Commit ncom messages, and consistency-check each commitment.
-	h := Head{}
-	for i := 0; i < ncom; i++ {
+	// Our update function simply collects and consistency-checks
+	// committed Heads until a designated time-step is reached.
+	up := func(h Head) (prop string, err error) {
+		//fmt.Printf("cli %v saw commit %v %q\n", self, h.Step, h.Data)
 
-		// Start the test client with appropriate parameters assuming
-		// n=3f, tr=2f, tb=f, and ts=f+1, satisfying TLCB's constraints.
-		pref := fmt.Sprintf("cli %v commit %v", self, i)
-		h = Commit(2*nfail, nfail+1, kv, rv, pref, h)
-		//println("thread", self, "got commit", h.Step, h.Data)
+		// Consistency-check the history h known to be committed
+		to.committed(t, h)
 
-		to.committed(t, h) // consistency-check history h
+		// Stop once we reach the step limit
+		if h.Step >= Step(maxstep) {
+			return "", errors.New("Done")
+		}
+
+		prop = fmt.Sprintf("cli %v proposal %v", self, h.Step)
+		return prop, nil
 	}
+
+	// Start the test client with appropriate parameters assuming
+	// n=3f, tr=2f, tb=f, and ts=f+1, satisfying TLCB's constraints.
+	c := Client{KV: kv, Tr: 2 * f, Ts: f + 1, Up: up, RV: rv}
+	c.Run()
+
 	wg.Done()
 }
 
 //  Run a consensus test case with the specified parameters.
-func testRun(t *testing.T, nfail, nnode, ncli, ncommits, maxpri int) {
+func testRun(t *testing.T, nfail, nnode, ncli, maxstep, maxpri int) {
 	desc := fmt.Sprintf("F=%v,N=%v,Clients=%v,Commits=%v,Tickets=%v",
-		nfail, nnode, ncli, ncommits, maxpri)
+		nfail, nnode, ncli, maxstep, maxpri)
 	t.Run(desc, func(t *testing.T) {
 
 		// Create a test key/value store representing each node
 		kv := make([]Store, nnode)
 		for i := range kv {
-			kv[i] = &testStore{kv: make(map[Step]Value)}
+			kv[i] = &testStore{}
 		}
 
 		// Create a reference total order for safety checking
@@ -101,26 +106,26 @@ func testRun(t *testing.T, nfail, nnode, ncli, ncommits, maxpri int) {
 		wg := &sync.WaitGroup{}
 		for i := 0; i < ncli; i++ {
 			wg.Add(1)
-			go testCli(t, i, nfail, ncommits, maxpri, kv, to, wg)
+			go testCli(t, i, nfail, maxstep, maxpri, kv, to, wg)
 		}
 		wg.Wait()
 	})
 }
 
 func TestClient(t *testing.T) {
-	testRun(t, 1, 3, 1, 1000, 100) // Standard f=1 case
-	testRun(t, 1, 3, 2, 1000, 100)
-	testRun(t, 1, 3, 10, 1000, 100)
-	testRun(t, 1, 3, 20, 1000, 100)
-	testRun(t, 1, 3, 50, 1000, 100)
-	testRun(t, 1, 3, 100, 1000, 100)
+	testRun(t, 1, 3, 1, 100000, 100) // Standard f=1 case
+	testRun(t, 1, 3, 2, 100000, 100)
+	testRun(t, 1, 3, 10, 100000, 100)
+	testRun(t, 1, 3, 20, 100000, 100)
+	testRun(t, 1, 3, 50, 100000, 100)
+	testRun(t, 1, 3, 100, 100000, 100)
 
-	testRun(t, 2, 6, 10, 1000, 100)  // Standard f=2 case
-	testRun(t, 3, 9, 10, 1000, 100)  // Standard f=3 case
-	testRun(t, 4, 12, 10, 1000, 100) // Standard f=4 case
-	testRun(t, 5, 15, 10, 1000, 100) // Standard f=10 case
+	testRun(t, 2, 6, 10, 10000, 100)  // Standard f=2 case
+	testRun(t, 3, 9, 10, 10000, 100)  // Standard f=3 case
+	testRun(t, 4, 12, 10, 10000, 100) // Standard f=4 case
+	testRun(t, 5, 15, 10, 10000, 100) // Standard f=10 case
 
 	// Test with low-entropy tickets: hurts commit rate, but still safe!
-	testRun(t, 1, 3, 10, 1000, 2) // Extreme low-entropy: rarely commits
-	testRun(t, 1, 3, 10, 1000, 3) // A bit better bit still bad...
+	testRun(t, 1, 3, 10, 10000, 2) // Extreme low-entropy: rarely commits
+	testRun(t, 1, 3, 10, 10000, 3) // A bit better bit still bad...
 }
